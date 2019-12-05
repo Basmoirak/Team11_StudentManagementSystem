543f0e81c7a02928ff2723a7965588eab60ac63d
Hein Htet Zaw <e0457819@u.nus.edu>
2019-12-05 11:51:48 +0800

    add department CRUD functionality

--- studentManagementSystem/src/main/java/com/team11/entity/Department.java
+++ studentManagementSystem/src/main/java/com/team11/entity/Department.java
@@ -1,12 +1,15 @@
 package com.team11.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Department {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int Id;
 	private String firstName;
 	
